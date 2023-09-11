ba7bd2eda5ed62dda823624e2efa269ae470aece
aum <aumkitti.01@gmail.com>
2023-09-11 09:51:04 +0700

    Update

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
+
 module.exports = {
-    HOST:"aws.connect.psdb.cloud",
-    USER:"16aghh7r1x9vr2j8o1a1",
-    PASSWORD:"pscale_pw_5b5aON8rVHEr9nqaiphCaGu4Ro4GO106TKTdukxpcUo",
-    DB:"se_database"
+    HOST:process.env.HOST,
+    USER:process.env.USER,
+    PASSWORD:process.env.PASSWORD,
+    DB:process.env.DB
 }
